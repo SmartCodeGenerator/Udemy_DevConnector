213d904b524ca97e95fd7125dd2db784e53e313b
Alex <alexmyt112@gmail.com>
2020-09-21 13:44:33 +0300

    Field of study check fixed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -248,7 +248,7 @@ router.put(
     auth,
     check('school', 'School is required').not().isEmpty(),
     check('degree', 'Degree is required').not().isEmpty(),
-    check('fieldofstudy', 'Field of study is required'),
+    check('fieldofstudy', 'Field of study is required').not().isEmpty(),
     check('from', 'From date is required').not().isEmpty(),
   ],
   async (req, res) => {
